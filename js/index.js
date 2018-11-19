782b28dd03b7de16c24ce8e06afb5030a29aaec8
Евгения Масликова < >
2018-11-19 19:09:03 +0300

    fixes rotate

--- js/index.js
+++ js/index.js
@@ -20,17 +20,13 @@ reset_button.disabled = true;
 solved_button.disabled = true;
 // creating pieces;
 function initial_state() { 
-    var mainWidth = parseInt(window.getComputedStyle(game_field).width.slice(0, -2));
-    var pieces_in_row = sets/4;
-    // var horizontal_row= mainWidth/pieces_in_row; 
-    for (var i = 0; i < sets; i++) {
+  for (var i = 0; i < sets; i++) {
         var game_pieces = document.createElement('div');
         game_pieces.className = 'item pos-' +i;
         game_pieces.setAttribute('id', i); 
         game_pieces.setAttribute('draggable', true);        
-        elements_blocks_array.push(game_pieces);              
-        // game_pieces.style.width = horizontal_row +'px';       
-        game_pieces.innerHTML = '<div class="item_wrapper"><div class="side back"><p class="symbol_text">3</p></div><div class="side front"><p class="number_text">1</p></div></div>';
+        elements_blocks_array.push(game_pieces); 
+        game_pieces.innerHTML = '<div class="side back"><p class="symbol_text">3</p></div><div class="side front"><p class="number_text">1</p></div>';
         
      }     
       // add to the the DOM pieces
@@ -38,7 +34,7 @@ function initial_state() {
          game_field.appendChild(elements_blocks_array[r]);
       }          
   }
- //setting games initia state
+ //setting games initial state
 initial_state();     
 function right_sequence(){
     var j =1;
@@ -82,43 +78,6 @@ function set_coodinates (){
    sets_position.push(coord); 
   }
 }
-//rotate pieces right
-function rotate_right(){
-for (var i=0; i<sets; i++){
- var index= parseInt(item_set[i].getAttribute('id'));
-  var new_index; 
-  if( index==11){
-    new_index =0;
-  }
-  else{
-    new_index = index+1;
-  }
-  setTimeout(function(){
-    item_set[i].style.left = sets_position[new_index][1]+ 'px';
-    item_set[i].style.top = sets_position[new_index][0]+ 'px';  
-       }, 2500)  
-  
-  item_set[i].setAttribute('id', new_index);
-  }  	
-}
-//rotate pieces left
-function rotate_left(){
-for (var i=0; i<sets; i++){
- var index= parseInt(item_set[i].getAttribute('id'));
-  var new_index; 
-  if( index==0){
-    new_index =11;
-  }
-  else{
-    new_index = index-1;
-  } 
-  setTimeout(function(){
-  item_set[i].style.left = sets_position[new_index][1]+ 'px';
-  item_set[i].style.top = sets_position[new_index][0]+ 'px';
-  }, 2500)    
-  item_set[i].setAttribute('id', new_index);
-  }  	
-}
 // Shaffle number funtion
 function shuffle(array) {	
   var currentIndex = array.length, temporaryValue, randomIndex ;  
@@ -131,12 +90,6 @@ function shuffle(array) {
   }
   return array;
 }
-
-//peventiong dbclicks
-game_field.dbclick = 
-function() { 
-  return false;
-} 
 //Mixing the numbers for new game
 function mix(){  
 // shuffle(numbers);
@@ -171,11 +124,9 @@ var wheel = document.getElementById('wheel');
 var wheelBox = wheel.getBoundingClientRect();
 var centerPoint = window.getComputedStyle(wheel).transformOrigin;
 var centers = centerPoint.split(" ");
-var rotate_lines_element =$('#top_line').find('.item');
+var rotate_lines_element =document.getElementsByClassName('changeble');
 var degrees=0// angle of rotation
-var initial_degrees =0;
-
-function rotateWheel(e) {
+function rotateWheel(e) { 
     var wheelEvent = e;
        if (e.targetTouches && e.targetTouches[0]) {
           e.preventDefault(); 
@@ -193,222 +144,134 @@ function rotateWheel(e) {
     // console.log(180-degrees);
     wheel.style.transform = 'rotate('+degrees+'deg)';
     top_line.style.transform = 'rotate('+degrees+'deg)';
-    number_text[0].style.transform = 'rotate('+degrees+'deg)';
-    number_text[1].style.transform = 'rotate('+degrees+'deg)';
-    number_text[2].style.transform = 'rotate('+degrees+'deg)';
-
-    
-
-   // var arr = [1,2,3,4,5,6];
-   // arr[1] = arr.splice(0,1, arr[1])[0];
-
-  
-   // console.log( elements_blocks_array);
-//console.log(arr)
 
     if(degrees>91 && degrees<181){
-        // alert("here");
-       	// var id_0 = item_set[0].getAttribute('id');
-       	// var id_2 = item_set[2].getAttribute('id');
-       	// item_set[2].setAttribute('id', id_0);
-       	// item_set[0].setAttribute('id', id_2);
-
-       	// alert(id_0);
-       	// alert(id_2);
-
-       	// var id_0 = number_text[0].innerText;
-       	// var id_2 = number_text[2].innerText;
-       	// number_text[2].innerText =  id_0;
-       	// number_text[0].innerText = id_2;
-
-
-
-    }
-        
+    }       
   
 } 
-
 var top_line_elements = [];
 var converted_to_arr = Array.prototype.slice.call(item_set);
 function append_top_line() {
-// console.log(top_line_elements);
-
 // find top_line pieces
   for (var j=0; j<12; j++){
       if($(converted_to_arr[j]).attr('id')<3) { 
+        $(converted_to_arr[j]).addClass('changeble');
       //fix them when rotating;
-         top_line_elements.push($(converted_to_arr[j]));
-         // console.log($(converted_to_arr[j]));
-
+        top_line_elements.push($(converted_to_arr[j]));
         top_line.appendChild(converted_to_arr[j]);
       }
-    }
-    // console.log(top_line_elements);
-    // var top_line_elements = [];
-    // var converted_to_arr= [];
-    // console.log(top_line_elements);
+    }    
 }
-// function append_top_line_field() {
-
-	// alert("here");
-    //  game_field.appendChild(item_set[0]);
-    // game_field.appendChild(item_set[1]);
-    // game_field.appendChild(item_set[2]);
-
-      // for (var j=0; j<3; j++){
-      //    game_field.appendChild(elements_blocks_array[j]);
-      // }
-// }
 function change_top_line() {
-
+  var changeble_numbers = document.getElementsByClassName('changeble');
+  var first_top_item = $(top_line_elements[0]).find('.number_text').text();
+  var third_item = $(top_line_elements[2]).find('.number_text').text();
+  $(top_line_elements[0]).find('.number_text').text(third_item);
+  $(top_line_elements[2]).find('.number_text').text(first_top_item);
+  
 	for (var j=0; j<12; j++){
-      if($(converted_to_arr[j]).attr('id')<3) { 
-      //fix them when rotating;
-         top_line_elements.push($(converted_to_arr[j]));
-         // console.log($(converted_to_arr[j]));
-
-        game_field.appendChild(converted_to_arr[j]);
+      if(converted_to_arr[j].getAttribute('id')<3) {
+       $(converted_to_arr[j]).removeClass('changeble');
+       game_field.appendChild(converted_to_arr[j]); 
       }
     }
+    //reset top_elements_set
+    top_line_elements =[];
+    degrees =0;
+    // alert(180-degrees);
+    rotated_degrees = 180-degrees;
+    top_line.style.transform = 'rotate('+ 0 +'deg)';
+}
 
-	// console.log(number_text[0].innerText);
-	// console.log(number_text[1].innerText);
-	// console.log(number_text[2].innerText);
-	// alert("here");
-    // for(var i; i<3; i++){
-    game_field.appendChild(item_set[0]);
-    game_field.appendChild(item_set[1]);
-    // game_field.appendChild(item_set[2]);
-    	// console.log(converted_to_arr[i]);
-    // }
-
-    // alert("Done");
-	
-	   // alert(degrees);
-       // if(degrees>91 && degrees<181){
-        // alert("here");
-       	// var id_0= set_item[0].getAttribute('id');
-       	// var id_2 = set_item[2].getAttribute('id');
-       	// alert(id_0);
-       	// alert(id_2);
-       	  // var first_top_item= number_text[0].innerText;          
-          // var second_top_item = number_text[2].innerText;
-          // console.log(elements_blocks_array);
-          // number_text[2].innerText=first_top_item;
-          // number_text[0].innerText=second_top_item;
-          // console.log(top_line_elements);
-          var first_top_item= item_set[0];          
-          var second_top_item = item_set[2];
-          item_set[0] = second_top_item
-          item_set[2] = first_top_item;
-          for (var i =0; i<3; i++){
-          	game_field.appendChild(top_line_elements[i]);
-          }
-          
-          console.log(top_line_elements);
-          // number_text[2].innerText=first_top_item;
-          // number_text[0].innerText=second_top_item;
-
-          // game_field.appendChild(item_set[0]);
-          // game_field.appendChild(item_set[1]);
-          // game_field.appendChild(item_set[2]);
-          
-
-
-          // }
-       }      
 top_pointer.addEventListener('touchmove', rotateWheel);
 top_pointer.addEventListener('touchstart', append_top_line, rotateWheel);
 top_pointer.addEventListener('touchend',  change_top_line);
-
-
-
-//defining a direction of set rotation
-var initial_touch;
-// item_set.addEventListener('touchstart', function(e) {
-//   initial_touch = e.originalEvent.touches[0].clientX;
-//   // console.log(initial_touch);
-// });
-
-// var currentX;
-// var lastX = 0;
-// var lastT;
-// $('.item').bind('touchstart', function(e) {
-//     // If still moving clear last setTimeout
-//     clearTimeout(lastT);
-//     var callback = function () {
-//         console.log(1);
-//     };
-
-//     initial_touch = e.originalEvent.touches[0].clientX;
-
-//     // After stoping or first moving
-//     if(lastX == 0) {
-//         lastX = initial_touch;
-//     }
-
-//     if(initial_touch < lastX) {
-//         // alert("hi left");
-//         rotate_left();
-//         callback();
-//     } else if(initial_touch > lastX){
-//         // alert("hi right");
-//         rotate_right();
-//         callback();
-//     }
-
-//     // Save last position
-//     lastX = initial_touch;
-
-//     // Check if moving is done
-//     lastT = setTimeout(function() {
-//         lastX = 0;
-//     }, 100);
-// });
-
-
-
-
-$('.item').bind('touchstart', function(e) {
-	// alert("here");
-    initial_touch = e.originalEvent.touches[0].clientX;
-    console.log(initial_touch);
-});
-
-$('.item').on('touchstart', function(e) {
-    var touch_offset = e.originalEvent.changedTouches[0].clientX;
-     // console.log(touch_offset);
-    if (initial_touch > touch_offset) {
-    //  setInterval(function() {
-      rotate_left();
-   // }, 1500);	
-     
-     } else {
-     	// alert("right");
-       rotate_right();
+//defult rotate direction 
+var touch_direction = 'left';
+//listen on player touch piece of game
+$('.item').on('touchstart', startTouchHandler);        
+$('.item').on('touchmove', moveTouchHandler);
+// defining ccordinates and rotate of pieces handlings
+var xDown = null;                                                        
+var yDown = null;                                                        
+function startTouchHandler(e) {                                      
+    xDown = e.originalEvent.touches[0].clientX;                                      
+    yDown = e.originalEvent.touches[0].clientY;                                      
+}; 
+function moveTouchHandler(e) { 
+    if ( ! xDown || ! yDown ) {
+        return;
     }
-});  
+    var xUp = e.originalEvent.touches[0].clientX;                                    
+    var yUp = e.originalEvent.touches[0].clientY;
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp; 
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 ) {
+          rotate_items('left');
+        } else {
+          rotate_items('right');
+        }  
+      } else {
+        if ( yDiff > 0 ) {
+           rotate_items('right'); 
+        } else { 
+           rotate_items('left');
+        }                                                                 
+    }
+      // to default                    
+    xDown = null;
+    yDown = null;                                             
+};
+//rotate pieces on particular direction
+function rotate_items(touch_direction){
+for (var i=0; i<sets; i++){
+var index= parseInt(item_set[i].getAttribute('id'));
+  var new_index; 
+  if( index==0 && touch_direction =='left'){
+    new_index =11;
+  }
+  if( index!=0 && touch_direction =='left'){
+    new_index = index-1;
+  }
+  if( index==11 && touch_direction =='right'){
+    new_index =0;
+  }
+    if( index!=11 && touch_direction =='right'){
+    new_index = index+1;
+  }  
+  item_set[i].style.left = sets_position[new_index][1]+ 'px';
+  item_set[i].style.top = sets_position[new_index][0]+ 'px'; 
+  // reset ids of pieces   
+  item_set[i].setAttribute('id', new_index);
+  }   
+}
+//buttons handlings 
 start_button.addEventListener('click', function() { 
   var count=0;
-  if(count>1){   
-    initial_state();
+  // alert(count);
+  if(count>0){  
+  location.reload(); 
+    // initial_state();
+    // ??????????????????????????????????????????????
     // game_disabled.style.display ='none';
-    $(document).find('.item').removeClass('flip'); 
-    $(document).find('.item').addClass('flip'); 
+   $(document).find('.item').addClass('flip'); 
+  
+    // $(document).find('.item').removeClass('flip'); 
+    // $(document).find('.item').addClass('flip'); 
     mix();      
   }
   else{
    mix();
+   // ??????????????????????????????????????????????
    $(document).find('.item').addClass('flip');  
    // game_disabled.style.display ='none';
-   count++;              
-  }
-   
+   count =1; 
+   alert(count);             
+  }   
    start_button.disabled = true;
    reset_button.disabled = false;
-   solved_button.disabled = false;  
-      
-
+   solved_button.disabled = false;
 });
 solved_button.addEventListener('click', function() {
   var solved_sequence = [];
@@ -420,6 +283,7 @@ solved_button.addEventListener('click', function() {
     $(number_text[i]).text(solved_sequence[i]);
     }  
     setInterval(function() {
+      // ??????????????????????????????????????????????
      $(document).find('.item').removeClass('flip');  
      }, 500);
      start_button.disabled = false;
@@ -433,33 +297,3 @@ reset_button.addEventListener('click', function() {
       $(number_text[i]).text(current_game_elements[i]);
     } 
 });
-
-
-// var currentX;
-// var lastX = 0;
-// var lastT;
-// $(document).bind('touchmove', function(e) {
-//     // If still moving clear last setTimeout
-//     clearTimeout(lastT);
-
-//     currentX = e.originalEvent.touches[0].clientX;
-
-//     // After stoping or first moving
-//     if(lastX == 0) {
-//         lastX = currentX;
-//     }
-
-//     if(currentX < lastX) {
-//         // Left
-//     } else if(currentX > lastX){
-//         // Right
-//     }
-
-//     // Save last position
-//     lastX = currentX;
-
-//     // Check if moving is done
-//     lastT = setTimeout(function() {
-//         lastX = 0;
-//     }, 100);
-// });
