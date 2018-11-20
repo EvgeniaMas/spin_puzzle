167029e99294177063e20b31ae6d79d6fc95ded2
Евгения Масликова < >
2018-11-20 22:53:50 +0300

    fixes more

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 var game_field = document.getElementById('numbers_field');//game board
 var symbols = document.getElementsByClassName('symbol_text');//symbols 
 var number_text = document.getElementsByClassName('number_text');//numbers opened
-var top_line =   document.getElementById('top_line');//rotation_line
+var top_line = document.getElementById('top_line');//rotation_line
 var elements_blocks_array = [];// created pieces on game board
 var numbers = []; //right sequence of numbers
 var sets_position =[];// game sets x,y
-var top_sets = [];// set of top line for rotation
 var current_game_elements =[]; //sequence for current game
 var item_set; //HTML collection of created items
 var sets = 12; //fixed amount of game set
-var config_data// configuration colors, symbols
+var config_data;// configuration colors, symbols
 var game_disabled = document.getElementById('game_disabled_layout');
 // buttons and state
 var start_button = document.getElementById('start_game');
@@ -22,7 +21,7 @@ solved_button.disabled = true;
 function initial_state() { 
   for (var i = 0; i < sets; i++) {
         var game_pieces = document.createElement('div');
-        game_pieces.className = 'item pos-' +i;
+        game_pieces.className = 'item';
         game_pieces.setAttribute('id', i); 
         game_pieces.setAttribute('draggable', true);        
         elements_blocks_array.push(game_pieces); 
@@ -43,6 +42,8 @@ function right_sequence(){
    } 
 }
 right_sequence();
+
+function set_symbols(){
 // checking for config file
 config_data = JSON.parse(data);
 var default_data = [
@@ -68,6 +69,8 @@ var default_data = [
       symbols[i].innerText = default_data[i][1];
      }
    }
+  }
+set_symbols();
 item_set = document.getElementsByClassName('item');
 document.addEventListener('DOMContentLoaded', set_coodinates);
 function set_coodinates (){
@@ -100,11 +103,11 @@ for (var i = 0; i <Math.ceil(numbers.length/color_size); i++){
 }
 var parts=[];
    //ascending array_parts
-    for (var i =0; i<numbers_parts.length; i++){
-      numbers_parts[i].sort(function (a, b) {
+    for (var k =0; k<numbers_parts.length; k++){
+      numbers_parts[k].sort(function (a, b) {
     return a - b;
      });
-      parts.push(numbers_parts[i]);
+      parts.push(numbers_parts[k]);
     }
     //new joined an ordered array to display
     numbers= parts.reduce(function(joined, current) {
@@ -124,8 +127,8 @@ var wheel = document.getElementById('wheel');
 var wheelBox = wheel.getBoundingClientRect();
 var centerPoint = window.getComputedStyle(wheel).transformOrigin;
 var centers = centerPoint.split(" ");
-var rotate_lines_element =document.getElementsByClassName('changeble');
 var degrees=0// angle of rotation
+var initial_rotation= 0;
 function rotateWheel(e) { 
     var wheelEvent = e;
        if (e.targetTouches && e.targetTouches[0]) {
@@ -140,64 +143,105 @@ function rotateWheel(e) {
     var centerY = wheelBox.top + parseInt(centers[1]) - window.pageYOffset;
     var centerX = wheelBox.left + parseInt(centers[0]) - window.pageXOffset;
     var radians = Math.atan2(mouseX - centerX, mouseY - centerY);
-    degrees = (radians * (180 / Math.PI) * -1) + 180;
-    // console.log(180-degrees);
+    degrees = (radians * (180 / Math.PI) * -1) + 180;   
     wheel.style.transform = 'rotate('+degrees+'deg)';
     top_line.style.transform = 'rotate('+degrees+'deg)';
 
-    if(degrees>91 && degrees<181){
-    }       
-  
 } 
 var top_line_elements = [];
 var converted_to_arr = Array.prototype.slice.call(item_set);
 function append_top_line() {
+ 
 // find top_line pieces
   for (var j=0; j<12; j++){
       if($(converted_to_arr[j]).attr('id')<3) { 
-        $(converted_to_arr[j]).addClass('changeble');
-      //fix them when rotating;
+       //fix them when rotating;
         top_line_elements.push($(converted_to_arr[j]));
         top_line.appendChild(converted_to_arr[j]);
       }
     }    
 }
-function change_top_line() {
-  var changeble_numbers = document.getElementsByClassName('changeble');
-  var first_top_item = $(top_line_elements[0]).find('.number_text').text();
-  var third_item = $(top_line_elements[2]).find('.number_text').text();
-  $(top_line_elements[0]).find('.number_text').text(third_item);
-  $(top_line_elements[2]).find('.number_text').text(first_top_item);
-  
-	for (var j=0; j<12; j++){
-      if(converted_to_arr[j].getAttribute('id')<3) {
-       $(converted_to_arr[j]).removeClass('changeble');
-       game_field.appendChild(converted_to_arr[j]); 
-      }
-    }
-    //reset top_elements_set
-    top_line_elements =[];
-    degrees =0;
-    // alert(180-degrees);
-    rotated_degrees = 180-degrees;
-    top_line.style.transform = 'rotate('+ 0 +'deg)';
-}
+//animate wheel rotation
+$.fn.animateWheel = function(angle, duration,deg) {
+  return this.each(function() {
+    var $rotate_elem = $(this);
+    $({deg: deg}).animate({deg: angle}, {
+      duration: duration,     
+      step: function(rotates) {
+        $rotate_elem.css({
+           transform: 'rotate(' + rotates + 'deg)'
+         });
+      },
+      
+    });
+  });
+};
 
-top_pointer.addEventListener('touchmove', rotateWheel);
-top_pointer.addEventListener('touchstart', append_top_line, rotateWheel);
-top_pointer.addEventListener('touchend',  change_top_line);
-//defult rotate direction 
-var touch_direction = 'left';
+function update_top_line(){
+//checking if top line is actual 
+ var first_top_item;
+ var third_item;
+ //updating HTML item collection
+  item_set = document.getElementsByClassName('item');
+  converted_to_arr = Array.prototype.slice.call(item_set);
+  for (var j=0; j<3; j++){   
+   if($(converted_to_arr[j]).attr('id')==0) { 
+    var first = j;
+    first_top_item = $(converted_to_arr[j]).find('.number_text').text(); 
+   }
+   if($(converted_to_arr[j]).attr('id')==2) { 
+    third_top_item = $(converted_to_arr[j]).find('.number_text').text(); 
+     var third = j;
+   }
+ }
+ var changeble_element1 = $(converted_to_arr[first]).find('.number_text');
+ var changeble_element2= $(converted_to_arr[third]).find('.number_text');
+ var color1 = $(changeble_element1).css('color'); 
+ var color2 = $(changeble_element2).css('color'); 
+ changeble_element1.text(third_top_item);
+ changeble_element2.text(first_top_item); 
+ changeble_element1.css('color', color2);
+ changeble_element2.css('color', color1); 
+
+  for(var i=0; i<12; i++){
+    game_field.appendChild(converted_to_arr[i]);
+  }  
+    //reset top_elements_set and degrees
+    top_line_elements =[];    
+    degrees =0;
+}
+function checkWheelposition() {
+//return top_line to default position checking the state of wheel
+if (initial_rotation==0){   
+   wheel.style.transform = 'rotate('+ 180 +'deg)'; 
+   update_top_line();
+   initial_rotation = 180;
+}
+else if (initial_rotation==180) {
+  wheel.style.transform = 'rotate('+ 0 +'deg)'; 
+  initial_rotation = 0;
+  update_top_line();
+ }
+}
+wheel.addEventListener('touchmove', rotateWheel);
+wheel.addEventListener('touchstart', append_top_line, rotateWheel);
+wheel.addEventListener('touchend', checkWheelposition);
 //listen on player touch piece of game
 $('.item').on('touchstart', startTouchHandler);        
 $('.item').on('touchmove', moveTouchHandler);
-// defining ccordinates and rotate of pieces handlings
+// defining ccordinates and rotate of pieces handlings and
+//target element
 var xDown = null;                                                        
-var yDown = null;                                                        
-function startTouchHandler(e) {                                      
-    xDown = e.originalEvent.touches[0].clientX;                                      
-    yDown = e.originalEvent.touches[0].clientY;                                      
-}; 
+var yDown = null;  
+var dragged_element;                                                      
+function startTouchHandler(e) {
+xDown = e.originalEvent.touches[0].clientX;                                      
+yDown = e.originalEvent.touches[0].clientY;   
+    dragged_element = e.target.parentNode;  
+    if (!$(dragged_element).hasClass('item')){
+     dragged_element = dragged_element.parentNode;
+    }                                 
+}
 function moveTouchHandler(e) { 
     if ( ! xDown || ! yDown ) {
         return;
@@ -206,24 +250,53 @@ function moveTouchHandler(e) {
     var yUp = e.originalEvent.touches[0].clientY;
     var xDiff = xDown - xUp;
     var yDiff = yDown - yUp; 
+    //getting target element id for directions;
+    var element_id=dragged_element.getAttribute('id'); 
+    // defining rotation direction deoending on id or default
     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-        if ( xDiff > 0 ) {
+        if ( xDiff > 0 && element_id>-1 && element_id<3) {         
           rotate_items('left');
-        } else {
+        } 
+        else if  ( xDiff > 0 && element_id>5 && element_id<9) {
+          rotate_items('right');           
+        }
+        else if ( xDiff < 0 && element_id>-1 && element_id<3) {
           rotate_items('right');
-        }  
+        } 
+        else if  ( xDiff < 0 && element_id>5 && element_id<9) {
+         
+          rotate_items('left');           
+        } 
+        else {          
+          rotate_items('right');
+        }
       } else {
-        if ( yDiff > 0 ) {
-           rotate_items('right'); 
-        } else { 
-           rotate_items('left');
+        // alert(element_id);
+        if ( yDiff > 0 && element_id>2 && element_id<6) {
+         
+            rotate_items('left');
+        } 
+        else if ( yDiff > 0 && element_id>8 && element_id<12) {
+          
+            rotate_items('right');
+        } 
+        else if( yDiff < 0 && element_id>2 && element_id<6) {
+         
+            rotate_items('right');
+        } 
+        else if ( yDiff < 0 && element_id>8 && element_id<12) {
+         
+            rotate_items('left');
+        } 
+        else { 
+          rotate_items('right');
         }                                                                 
     }
       // to default                    
     xDown = null;
     yDown = null;                                             
-};
-//rotate pieces on particular direction
+}
+//rotate pieces in particular direction
 function rotate_items(touch_direction){
 for (var i=0; i<sets; i++){
 var index= parseInt(item_set[i].getAttribute('id'));
@@ -247,33 +320,28 @@ var index= parseInt(item_set[i].getAttribute('id'));
   }   
 }
 //buttons handlings 
-start_button.addEventListener('click', function() { 
-  var count=0;
-  // alert(count);
-  if(count>0){  
-  location.reload(); 
-    // initial_state();
-    // ??????????????????????????????????????????????
-    // game_disabled.style.display ='none';
+var count=0;
+
+$(start_button).on('click', function() {
+  if (count>0){
+  initial_state(); 
+  set_symbols();
+  mix(); 
+   game_disabled.style.display ='none';
    $(document).find('.item').addClass('flip'); 
-  
-    // $(document).find('.item').removeClass('flip'); 
-    // $(document).find('.item').addClass('flip'); 
-    mix();      
   }
-  else{
-   mix();
-   // ??????????????????????????????????????????????
-   $(document).find('.item').addClass('flip');  
-   // game_disabled.style.display ='none';
-   count =1; 
-   alert(count);             
-  }   
+  else { 
+
+   mix(); 
+   game_disabled.style.display ='none';
+   $(document).find('.item').addClass('flip');
+   count=1; 
+   }  
    start_button.disabled = true;
    reset_button.disabled = false;
    solved_button.disabled = false;
 });
-solved_button.addEventListener('click', function() {
+$(solved_button).on('click', function() {
   var solved_sequence = [];
   var j =1;
      while (j <= sets) {
@@ -281,19 +349,22 @@ solved_button.addEventListener('click', function() {
    } 
   for (var i = 0; i < solved_sequence.length; i++){ 
     $(number_text[i]).text(solved_sequence[i]);
-    }  
-    setInterval(function() {
-      // ??????????????????????????????????????????????
-     $(document).find('.item').removeClass('flip');  
-     }, 500);
+    } 
+   $(document).find('.item').addClass('flip');
+   $(document).find('.item').removeClass('flip');
      start_button.disabled = false;
      reset_button.disabled = true;
      solved_button.disabled = true;
      game_disabled.style.display ='block';
-
 });
-reset_button.addEventListener('click', function() { 
+$(reset_button).on('click', function() { 
   for (var i = 0; i < current_game_elements.length; i++){ 
       $(number_text[i]).text(current_game_elements[i]);
     } 
+  wheel.style.transform = 'rotate('+degrees+'deg)'; 
+
+  // for (var i = 0; i < 12; i++){ 
+  // $(converted_to_arr[i]).find('.number_text').text(current_game_elements[i]);
+  // game_field.appendChild(converted_to_arr[i]);    
+  // }  
 });
