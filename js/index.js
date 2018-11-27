e9d5bb79fc322c85924f62d21bdcd556e3342418
Евгения Масликова < >
2018-11-27 18:42:19 +0300

    check function

--- js/index.js
+++ js/index.js
@@ -10,6 +10,10 @@ var item_set; //HTML collection of created items
 var sets = 12; //fixed amount of game set
 var config_data;// configuration colors, symbols
 var game_disabled = document.getElementById('game_disabled_layout');
+var blue =[], green =[], orange =[]; // color sequenses (just three, color indipendent)
+var unique_colors;//config unique colors values
+var data; 
+var current_sequence = [];
 // buttons and state
 var start_button = document.getElementById('start_game');
 var reset_button = document.getElementById('reset');
@@ -35,17 +39,22 @@ function initial_state() {
   }
  //setting games initial state
 initial_state();     
-function right_sequence(){
+function normal_number_sequence(){
     var j =1;
      while (j <= sets) {
      numbers.push(j++);
    } 
 }
-right_sequence();
 
+normal_number_sequence();
+var config_colors =[];
+var config_sequence =[];
 function set_symbols(){
 // checking for config file
-config_data = JSON.parse(data);
+if(data){
+ config_data = JSON.parse(data);  
+}
+
 var default_data = [
       ['#91ebf8', '#'],['#91ebf8', '2'],
       ['#91ebf8', '3'],['#91ebf8', '3'],
@@ -54,25 +63,64 @@ var default_data = [
       ['#90c881', '#'],['#90c881', '2'],
       ['#90c881', '3'],['#90c881', '3']
     ]; 
+
 // TO USE CONFIG FILE DELETE "!", SO (config_data)   
-  if(!config_data){ 
+  if(config_data){ 
   for (var i=0; i<sets; i++){   
       symbols[i].style.color = config_data[i].color;
       number_text[i].style.color = config_data[i].color;      
       symbols[i].innerText = config_data[i].symbol;
+      number_text[i].innerText = config_data[i].number;
+      config_colors.push(config_data[i].color);
+      config_sequence[i] = config_data[i].number;      
      }
   }
    else{
     for (var i=0; i<sets; i++){   
       symbols[i].style.color = default_data[i][0];
-      number_text[i].style.color = default_data[i][0];      
+      number_text[i].style.color = default_data[i][0];
       symbols[i].innerText = default_data[i][1];
      }
    }
-  }
+unique_colors = unique(config_colors);
+}
 set_symbols();
+//get unique colors in config; 
+function unique(colors) {
+  var obj_color = {};
+  for (var i = 0; i < colors.length; i++) {
+    var values = colors[i];
+    obj_color[values] = true; 
+  }
+  return Object.keys(obj_color); 
+}
+//get color/number sequences in current game
+function set_color_sequences(){
+if(config_data){
+  alert("Config");
+   for (var i=0; i<sets; i++){
+      if(config_data[i].color == unique_colors[0]){
+    blue.push(config_data[i].number);
+    }
+      else if(config_data[i].color == unique_colors[1]){
+     green.push(config_data[i].number);
+    }
+      else if(config_data[i].color == unique_colors[2]){
+    orange.push(config_data[i].number);
+    }
+  }
+}
+  
+else{
+   alert("No config");
+   blue = numbers.slice(0, 4);
+   green = numbers.slice(4, 8);
+   orange = numbers.slice(8, 12); 
+ }  
+}
 item_set = document.getElementsByClassName('item');
 document.addEventListener('DOMContentLoaded', set_coodinates);
+
 function set_coodinates (){
  for (i=0; i<sets; i++){
    var posX = item_set[i].offsetTop; 
@@ -95,24 +143,7 @@ function shuffle(array) {
 }
 //Mixing the numbers for new game
 function mix(){  
-// shuffle(numbers);
-var color_size = 4; 
-var numbers_parts = []; 
-for (var i = 0; i <Math.ceil(numbers.length/color_size); i++){
-    numbers_parts[i] = numbers.slice((i*color_size), (i*color_size) + color_size);
-}
-var parts=[];
-   //ascending array_parts
-    for (var k =0; k<numbers_parts.length; k++){
-      numbers_parts[k].sort(function (a, b) {
-    return a - b;
-     });
-      parts.push(numbers_parts[k]);
-    }
-    //new joined an ordered array to display
-    numbers= parts.reduce(function(joined, current) {
-     return joined.concat(current);
-    }, []);
+shuffle(numbers);  
    item_set = document.getElementsByClassName('item');
    current_game_elements = numbers;
    display();
@@ -120,7 +151,9 @@ var parts=[];
 function display(){
    for (var i = 0; i < numbers.length; i++){ 
        number_text[i].innerText= numbers[i];
-    }  
+    } 
+    set_color_sequences(); 
+    get_current_sequence();
 }
 // rotation elements
 var wheel = document.getElementById('wheel');
@@ -150,8 +183,7 @@ function rotateWheel(e) {
 } 
 var top_line_elements = [];
 var converted_to_arr = Array.prototype.slice.call(item_set);
-function append_top_line() {
- 
+function append_top_line() { 
 // find top_line pieces
   for (var j=0; j<12; j++){
       if($(converted_to_arr[j]).attr('id')<3) { 
@@ -161,29 +193,14 @@ function append_top_line() {
       }
     }    
 }
-//animate wheel rotation
-$.fn.animateWheel = function(angle, duration,deg) {
-  return this.each(function() {
-    var $rotate_elem = $(this);
-    $({deg: deg}).animate({deg: angle}, {
-      duration: duration,     
-      step: function(rotates) {
-        $rotate_elem.css({
-           transform: 'rotate(' + rotates + 'deg)'
-         });
-      },
-      
-    });
-  });
-};
-
 function update_top_line(){
 //checking if top line is actual 
  var first_top_item;
  var third_item;
  //updating HTML item collection
   item_set = document.getElementsByClassName('item');
-  converted_to_arr = Array.prototype.slice.call(item_set);
+  converted_to_arr = Array.prototype.slice.call(item_set);  
+
   for (var j=0; j<3; j++){   
    if($(converted_to_arr[j]).attr('id')==0) { 
     var first = j;
@@ -198,29 +215,42 @@ function update_top_line(){
  var changeble_element2= $(converted_to_arr[third]).find('.number_text');
  var color1 = $(changeble_element1).css('color'); 
  var color2 = $(changeble_element2).css('color'); 
+  // console.log(current_sequence);
+
+  var index1 = current_sequence.indexOf(first_top_item);
+  var index2 = current_sequence.indexOf(third_top_item);
+
+  // // if (index !== -1) {
+    current_sequence[index1] = third_top_item;
+    current_sequence[index2] = first_top_item;
+  //}
+ console.log(current_sequence);
  changeble_element1.text(third_top_item);
  changeble_element2.text(first_top_item); 
  changeble_element1.css('color', color2);
  changeble_element2.css('color', color1); 
-
-  for(var i=0; i<12; i++){
+ for(var i=0; i<12; i++){
     game_field.appendChild(converted_to_arr[i]);
-  }  
-    //reset top_elements_set and degrees
+       
+  } 
     top_line_elements =[];    
     degrees =0;
+  
+    check_player_solution();
 }
 function checkWheelposition() {
 //return top_line to default position checking the state of wheel
 if (initial_rotation==0){   
    wheel.style.transform = 'rotate('+ 180 +'deg)'; 
    update_top_line();
+    
    initial_rotation = 180;
-}
-else if (initial_rotation==180) {
+ }
+ else if (initial_rotation==180) {
   wheel.style.transform = 'rotate('+ 0 +'deg)'; 
   initial_rotation = 0;
   update_top_line();
+    
  }
 }
 wheel.addEventListener('touchmove', rotateWheel);
@@ -294,10 +324,12 @@ function moveTouchHandler(e) {
     }
       // to default                    
     xDown = null;
-    yDown = null;                                             
+    yDown = null; 
+                                             
 }
 //rotate pieces in particular direction
 function rotate_items(touch_direction){
+   // current_sequence=[];
 for (var i=0; i<sets; i++){
 var index= parseInt(item_set[i].getAttribute('id'));
   var new_index; 
@@ -313,31 +345,21 @@ var index= parseInt(item_set[i].getAttribute('id'));
     if( index!=11 && touch_direction =='right'){
     new_index = index+1;
   }  
+  item_set[i].style.transition = '1.5s ease-in-out';
   item_set[i].style.left = sets_position[new_index][1]+ 'px';
   item_set[i].style.top = sets_position[new_index][0]+ 'px'; 
+  
   // reset ids of pieces   
   item_set[i].setAttribute('id', new_index);
-  }   
+  } 
 }
+ 
 //buttons handlings 
-var count=0;
-
 $(start_button).on('click', function() {
-  // if (count>0){
-  // initial_state(); 
-  // set_symbols();
-  // mix(); 
-  //  game_disabled.style.display ='none';
-  //  $(document).find('.item').addClass('flip'); 
-  // }
-  // else { 
-
    mix(); 
    game_disabled.style.display ='none';
    $(document).find('.item').addClass('flip');
    set_symbols();
-   count=1; 
-   // }  
    start_button.disabled = true;
    reset_button.disabled = false;
    solved_button.disabled = false;
@@ -362,10 +384,54 @@ $(reset_button).on('click', function() {
   for (var i = 0; i < current_game_elements.length; i++){ 
       $(number_text[i]).text(current_game_elements[i]);
     } 
-  wheel.style.transform = 'rotate('+degrees+'deg)'; 
-
-  // for (var i = 0; i < 12; i++){ 
-  // $(converted_to_arr[i]).find('.number_text').text(current_game_elements[i]);
-  // game_field.appendChild(converted_to_arr[i]);    
-  // }  
+  wheel.style.transform = 'rotate('+degrees+'deg)';
 });
+//sort asc function 
+ function sort_asc(arr) {
+   return arr.concat().sort(function (a, b) {
+     return a - b;
+ });
+}
+//get acsending clockwise color sequences
+var blue_asc;
+var green_asc;
+var orange_asc;
+function check_player_solution(){
+var joined_sequence = current_sequence.join('');
+// console.log(current_sequence);
+blue_asc = sort_asc(blue).join('');
+green_asc = sort_asc(green).join('');
+orange_asc = sort_asc(orange).join('');
+console.log(joined_sequence);
+console.log(current_sequence.indexOf(blue_asc));
+console.log(blue_asc);
+
+//checking up all three color-number counterclock solutions
+if (joined_sequence.indexOf(blue_asc)>-1){
+// && joined_sequence.indexOf(green_asc)>-1
+// && joined_sequence.indexOf(orange_asc)>-1 
+  console.log("Solved");  
+  //flipping items  
+  setTimeout(function() { $(document).find('.item').removeClass('flip'); }, 1000);
+   //change buttons state
+   start_button.disabled = false;
+   reset_button.disabled = true;
+   solved_button.disabled = true;
+  }
+
+}
+
+function get_current_sequence(){
+if(data) {
+   current_sequence = config_sequence;
+   console.log(current_sequence);  
+}
+else{
+  current_sequence =[];
+  var arr = [].slice.call(number_text);
+  for (i=0; i<12; i++) {
+    current_sequence.push(arr[i].innerText);
+  }
+  console.log(current_sequence);  
+  }
+}
